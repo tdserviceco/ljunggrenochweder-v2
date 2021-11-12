c9c6b83d36fcac43c464e587bd50afc07c95ca3a
Tommy Danielsson <tdserviceco@gmail.com>
2021-11-12 22:25:36 +0100

    added new stuff to sass and login works now with google

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,30 @@ import React from 'react';
 import { GoogleLogin } from 'react-google-login';
 import { useNavigate } from 'react-router-dom';
 import { Icon } from '@iconify/react';
+import Header from '../components/header/Header';
+import Footer from '../components/footer/footer';
+import axios from 'axios';
 const Home = () => {
   const navigate = useNavigate()
   const responseGoogle = (response) => {
-    if (response.profileObj.googleId && response.profileObj.email) {
-      navigate('/registration')
+    if (response.profileObj.googleId) {
+      console.log(typeof (response.profileObj.email))
+      const result = axios.post(`http://localhost:9000/.netlify/functions/api/v1/google-auth`, { email: response.profileObj.email })
+      result.then(res => {
+        console.log(res.data)
+      }).catch(err => {
+        if (err.response) {
+          navigate('/registration')
+        }
+      })
     }
   }
 
   return (
-    <div>
-      Home
+    <section className="home">
+      <Header />
       <GoogleLogin
-        clientId="920638617112-vrr044b57bqkhic1bvmqnhch7459j2kr.apps.googleusercontent.com"
+        clientId={import.meta.env.VITE_APP_GID}
         buttonText="Login"
         render={renderProps => (
           <button onClick={renderProps.onClick} disabled={renderProps.disabled}><Icon icon="flat-color-icons:google" />This is my custom Google button</button>
@@ -23,7 +34,8 @@ const Home = () => {
         onFailure={responseGoogle}
         cookiePolicy={'single_host_origin'}
       />
-    </div>
+      <Footer />
+    </section>
   );
 };
 
