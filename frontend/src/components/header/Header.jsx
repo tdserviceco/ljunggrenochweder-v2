d1949fcf1d4a49a3e79c960ec7d0993f0dcb9034
tim.varvikko <70330995+Varvikko455@users.noreply.github.com>
2022-02-04 12:20:32 +0100

    Hamburger menu animation done

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,19 +1,17 @@
 import React, { useState } from 'react';
-import { Login } from '..';
-import { Register } from '..';
+import { Login, Register } from '..';
 
 const Header = () => {
 
   const [login, toggleLogin] = useState(false);
   const [register, toggleRegister] = useState(false);
+  const [hamburger, toggleHamburger] = useState(false)
 
   return (
     <header>
       <div className='header-wrapper'>
-        <div className='hamburger-menu'>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div onClick={() => toggleHamburger(!hamburger)} className={`menu-btn`}>
+          <div className={`menu-btn__burger ${hamburger ? 'open' : ''}`}></div>
         </div>
         <div className='title-and-log-reg'>
           <h3 className='title'>Ljungren & Weder</h3>
