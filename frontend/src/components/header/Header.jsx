864fa607fdde88a775a86618805f2328a819ed7d
Tdserviceco <tdserviceco@gmail.com>
2022-02-10 16:37:12 +0100

    random avartar pictures for fun

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -4,9 +4,30 @@ import { Login, Register } from '..';
 import NavigationMenu from './NavigationMenu';
 import { useCookies } from 'react-cookie';
 import { Icon } from '@iconify/react';
+import { useEffect } from 'react';
 
 
 const Header = () => {
+  const [avartars] = useState([
+    { image: "/img/atlas_beetle" },
+    { image: "/img/puffin" },
+    { image: "/img/scorpion" },
+    { image: "/img/toad" },
+    { image: "/img/unicorn" },
+    { image: "/img/tyrannosaurus" }
+  ])
+
+  const [profile, setProfile] = useState('')
+
+  useEffect(() => {
+    /** inspiration: https://stackoverflow.com/questions/33443122/display-random-images-using-a-foreach-loop-and-and-one-img-tag */
+    const avartar = document.querySelector('.avartar');
+    console.log(avartar)
+    for (let i = 0; i < avartars.length; i++) {
+      let randomNum = Math.floor(Math.random() * avartars.length);
+      return setProfile(avartars[randomNum]);
+    }
+  }, [])
 
   const [cookies] = useCookies(['userProfile']);
 
@@ -35,16 +56,16 @@ const Header = () => {
     preEnter: true,
     preExit: true
   });
-  
-  const loginButton =  () => {
+
+  const loginButton = () => {
     toggleLogin(!login);
     toggleLoginEffect();
   };
-  const registerButton =  () => {
+  const registerButton = () => {
     toggleRegister(!register);
     toggleRegisterEffect();
   };
-  const hamburgerButton =  () => {
+  const hamburgerButton = () => {
     toggleHamburger(!hamburger);
     toggleNavigationEffect();
   };
@@ -61,24 +82,24 @@ const Header = () => {
         </div>
         <div className='title-and-log-reg'>
           <h3 className='title'>Ljungren & Weder</h3>
-          {cookies.userProfile === undefined ? 
+          {cookies.userProfile === undefined ?
             <div className="log-reg-container">
               <button className={`login-button ${register ? 'disable' : ''}`} onClick={() => loginButton()}>Logga in </button>
               <span>/</span>
-              <button className={`register-button ${login ? 'disable' : ''}`} onClick={() => registerButton() }>Registrera</button>
+              <button className={`register-button ${login ? 'disable' : ''}`} onClick={() => registerButton()}>Registrera</button>
             </div> :
             <div className='user-avatar'>
-              
-              <img  src="/img/atlas_beetle.png" alt="animal" />
+              {/* Loop time baby */}
+              <img className="avartar" src={`${profile.image}.png`} alt="animal" />
               <h3>{cookies.userProfile.username}</h3>
-            </div> 
+            </div>
           }
         </div>
       </div>
 
-      {!showNavigation && <NavigationMenu state={ navigationEffect }/>}
-      {!showLogin && <Login state={ loginEffect }/>}
-      {!showRegister && <Register state={ registerEffect }/>}
+      {!showNavigation && <NavigationMenu state={navigationEffect} />}
+      {!showLogin && <Login state={loginEffect} />}
+      {!showRegister && <Register state={registerEffect} />}
     </header>
   );
 };
